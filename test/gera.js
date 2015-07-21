f4a6ad12d49a65d0466ff2a42b125a826f62539c
thr0w <josefernandotolentino@gmail.com>
2015-07-21 07:53:33 -0300

    spec test gerado

--- test/gera.js
+++ test/gera.js
@@ -5,8 +5,13 @@ function gera_arquivo(arq, dados) {
     var conteudo = fs.readFileSync(__dirname + '/template/' + arq, {
         encoding: 'utf8'
     });
-    var conteudo_gerado = conteudo.replace('$$mock$$', JSON.stringify(dados.mock, null, 2));
-    fs.writeFileSync(path.resolve(__dirname + '/../temp/' + arq), conteudo_gerado, {
+    var conteudo_gerado_view = conteudo.replace('$$mock$$', JSON.stringify(dados.mock, null, 2));
+    fs.writeFileSync(path.resolve(__dirname + '/../temp/' + arq), conteudo_gerado_view, {
+        encoding: 'utf8'
+    });
+
+    var conteudo_gerado_spec = conteudo.replace('$$btn_salvar$$', JSON.stringify(dados.mock, null, 2));
+    fs.writeFileSync(path.resolve(__dirname + '/../temp/' + arq), conteudo_gerado_spec, {
         encoding: 'utf8'
     });
 }
--- test/library.js
+++ test/library.js
@@ -16,94 +16,97 @@ module.exports = (function () {
     var library = Portuguese.library()
         .define('ESTADO', /[^\u0000]*/)
         .given('que eu tenho uma ação com o $ESTADO', function (estado, next) {
-            debugger;
             mock_store = {
                 salvar: estado
             }
             next();
         })
         .when('eu renderizar o action', function (next) {
-
-
-            //            var index = [
-            //            '<html>' + '\n' +
-            //            '    <head>' + '\n' +
-            //            '        <meta charset="utf-8">' + '\n' +
-            //            '    </head>' + '\n' +
-            //            '    <body id="app">' + '\n' +
-            //            '        <script src="bundle.js"></script>' + '\n' +
-            //            '    </body>' + '\n' +
-            //            '</html>
-            //            ];
-            //            var view = [
-            //            'require React from "react";' + '\n' +
-            //            'function createViewTest()' + '\n' +
-            //            '{' + '\n' +
-            //            '  return React.createElement(H5Action, {' + '\n' +
-            //            '       store: ' + mock_store + '},' + '\n' +
-            //            '       action: "salvar"' + '\n' +
-            //            '});'
-            //            ];
-            //            var webpack = [
-            //                'var webpack = require('webpack');'
-            //
-            //                'module.exports = {'
-            //                    'entry: __dirname + "/action.view.js",'
-            //                    'output: {'
-            //                        'path: __dirname,'
-            //                        'filename: "bundle.js"'
-            //                    '},'
-            //                    'module: {'
-            //        '               loaders: ['
-            //                        '{'
-            //                            'test: /\.less$/,'
-            //                            'loader: "style!css!less"'
-            //                        '},'
-            //                        '{'
-            //                            'test: /\.jsx?$/,'
-            //                            'loaders: ["jsx?harmony&stripTypes", "flowcheck"],'
-            //                            'exclude: /node_modules/'
-            //                        '},'
-            //                        '{'
-            //                            'test: /\.json$/,'
-            //                            'loader: "json"'
-            //                        '}'
-            //                        ']'
-            //    '},'
-            //    'devtool: "source-map",'
-            //    'devServer: {'
-            //        'contentBase: __dirname,'
-            //        'hot: false,'
-            //        'inline: true,'
-            //        'noInfo: false,'
-            //        'host: "0.0.0.0",'
-            //        'port: 3080,'
-            //        'colors: true'
-            //    '},'
-            //    'plugins: ['
-            //        'new webpack.optimize.MinChunkSizePlugin({'
-            //            'minChunkSize: 100000000'
-            //        '})'
-            //    ']'
-            //'};'
-            //
-            //
-            //            ];
-            //            filewrite('gerado/app/index.html', index);
-            //            filewrite('gerado/app/action.view.js', view);
-            //            filewrite('gerado/app/webpack.config.js', webpack);
-            //
-            //            run('webpack-dev-server');
-            debugger;
             gera.gera_arquivo('app/action.view.js', {
                 mock: mock_store
             });
             next();
         })
-        .then('validar o css [spec]', function (spec) {
-            filewrite('gerado/galen/teste_inicial.spec', spec)
+        .define('SPEC', /[^\u0000]*/)
+        .then('validar o css $SPEC', function (spec) {
+            gera.gera_arquivo('galen/teste_inicial.spec', {
+                btn_salvar: spec
+            });
             run(galen);
         });
     return library;
 }());
 //---------------------------------------------------------------------------
+
+
+
+
+//            var index = [
+//            '<html>' + '\n' +
+//            '    <head>' + '\n' +
+//            '        <meta charset="utf-8">' + '\n' +
+//            '    </head>' + '\n' +
+//            '    <body id="app">' + '\n' +
+//            '        <script src="bundle.js"></script>' + '\n' +
+//            '    </body>' + '\n' +
+//            '</html>
+//            ];
+//            var view = [
+//            'require React from "react";' + '\n' +
+//            'function createViewTest()' + '\n' +
+//            '{' + '\n' +
+//            '  return React.createElement(H5Action, {' + '\n' +
+//            '       store: ' + mock_store + '},' + '\n' +
+//            '       action: "salvar"' + '\n' +
+//            '});'
+//            ];
+//            var webpack = [
+//                'var webpack = require('webpack');'
+//
+//                'module.exports = {'
+//                    'entry: __dirname + "/action.view.js",'
+//                    'output: {'
+//                        'path: __dirname,'
+//                        'filename: "bundle.js"'
+//                    '},'
+//                    'module: {'
+//        '               loaders: ['
+//                        '{'
+//                            'test: /\.less$/,'
+//                            'loader: "style!css!less"'
+//                        '},'
+//                        '{'
+//                            'test: /\.jsx?$/,'
+//                            'loaders: ["jsx?harmony&stripTypes", "flowcheck"],'
+//                            'exclude: /node_modules/'
+//                        '},'
+//                        '{'
+//                            'test: /\.json$/,'
+//                            'loader: "json"'
+//                        '}'
+//                        ']'
+//    '},'
+//    'devtool: "source-map",'
+//    'devServer: {'
+//        'contentBase: __dirname,'
+//        'hot: false,'
+//        'inline: true,'
+//        'noInfo: false,'
+//        'host: "0.0.0.0",'
+//        'port: 3080,'
+//        'colors: true'
+//    '},'
+//    'plugins: ['
+//        'new webpack.optimize.MinChunkSizePlugin({'
+//            'minChunkSize: 100000000'
+//        '})'
+//    ']'
+//'};'
+//
+//
+//            ];
+//            filewrite('gerado/app/index.html', index);
+//            filewrite('gerado/app/action.view.js', view);
+//            filewrite('gerado/app/webpack.config.js', webpack);
+//
+//            run('webpack-dev-server');
