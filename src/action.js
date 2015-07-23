0bf2e6a779fa2514d64d1a887158b4f0e28d994d
thr0w <josefernandotolentino@gmail.com>
2015-07-23 08:49:40 -0300

    spec tests

--- src/action.js
+++ src/action.js
@@ -18,19 +18,19 @@ var HAction = React.createClass({
     render: function () {
         var store = this.props.store;
         var action = store[this.props.action];
-        var kind = this.props.kind;
+        var kind = action.kind;
         var props = {};
         props.label = action.labelText;
         props.onTouchTap = this._click;
         props.className = 'h_action ' + (this.state.clickedButton == this ? 'h_action_clicked' : '');
-        if (this.state.clickedButton == '' && kind == 'normal')
-            props.className = 'h_action h_action_kind_normal';
-        else if (this.state.clickedButton == '' && kind == 'primary')
-            props.className = 'h_action h_action_kind_primary';
-        else if (this.state.clickedButton == '' && kind == 'secondary')
-            props.className = 'h_action h_action_kind_secondary';
-        else if (this.state.clickedButton == '' && kind == 'tertiary')
-            props.className = 'h_action h_action_kind_tertiary';
+        if (kind == 'normal')
+            props.className = 'h_action_kind_normal';
+        else if (kind == 'primary')
+            props.className = 'h_action_kind_primary';
+        else if (kind == 'secondary')
+            props.className = 'h_action_kind_secondary';
+        else if (kind == 'tertiary')
+            props.className = 'h_action_kind_tertiary';
         return (React.createElement("button", props, [action.labelText]));
     },
     _click: function (e) {
--- test/template/test/teste_inicial.spec
+++ test/template/test/teste_inicial.spec
@@ -1,6 +1,7 @@
 ===========================================
-btn_salvar   css    button[label = 'Salvar']
-body         id     app
+btn_salvar       css    button[label = 'Salvar']
+body             id     app
+class_btn_salvar css   button[class = 'h_action_kind_normal']
 ===========================================
 
 ___specSalvar___
